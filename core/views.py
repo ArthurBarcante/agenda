0a4e975ccd0ec3d12d612f0d8ad85c9264db1620
arthurbarcante <arthurinformatica2022@gmail.com>
2025-08-28 11:02:23 -0300

    Fazendo uma espécie de pesquisa de eventos criados através da URL e fazendo uma página inicial com Hello, World

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Evento
 
 # Create your views here.
+def hello(request):
+    return HttpResponse("Hello World!!!!")
+
+def eventos(request, titulo_evento):
+    consulta = Evento.objects.get(titulo=titulo_evento)
+    data = Evento.objects.get(titulo=titulo_evento).data_evento
+    if consulta:
+        return HttpResponse("{}, Data: {}". format(consulta, data))
+    return HttpResponse("Evento não encontrado")
